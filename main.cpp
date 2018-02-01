85d06e4832fcfd55cb58cba47bd87f983860404e
Biwel <jakub.biwel@gmail.com>
2018-02-01 16:14:50 +0100

    Some experiment to get information about network interfaces and their state.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
-#include <QApplication>
 #include <QPushButton>
+#include <QtNetwork>
+#include <QDebug>
 
 #include "ui_openweathermain.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    QWidget w;
+    QNetworkConfigurationManager *ncm = new QNetworkConfigurationManager();
+    if (!ncm->isOnline()) {
+         qDebug() << "Device is NOT online" << endl;
+         delete ncm;
+         return -1;
+    }
 
-    Ui::Form ui;
-    ui.setupUi(&w);
-    w.showFullScreen();
-
-    return a.exec();
+    QList<QNetworkConfiguration> netConfs;
+    netConfs = ncm->allConfigurations();
+    qDebug() << "Got "<< netConfs.size() << " active configurations" << endl;
+    for (int i = 0; i < netConfs.size(); i++) {
+        QNetworkConfiguration nc = netConfs.at(i);
+        qDebug() << nc.identifier() << "of type" << nc.bearerType() << nc.bearerTypeName() << " name" << nc.name() <<
+                  " state" << (0xe == nc.state()? " active" : " not active");
+    }
 }
