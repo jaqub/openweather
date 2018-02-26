7c6a38314edcf5bbc4851dd794fb3fd8e99866a9
jaqub <jakub.biwel@gmail.com>
2018-02-26 18:40:57 +0100

    [SRC] Application input params parser added.
    Use of QCommandLineParser has been added in order to
    parse mandatory input params.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include <QApplication>
+#include <QCommandLineParser>
 
 #include "openweather.h"
 
 int main(int argc, char *argv[])
 {
-    int ret;
-
     QApplication a(argc, argv);
+    QCommandLineParser parser;
 
-    OpenWeather *openWeather = new OpenWeather(nullptr, argv[1]);
+    QCommandLineOption appIdOpt("appid",
+      QApplication::translate("main", "api key obtainted from http://openweathermap.org"), "api key");
+    parser.addOption(appIdOpt);
 
+    QCommandLineOption idOpt("id", QApplication::translate("main", "city id"), "city id");
+    parser.addOption(idOpt);
+
+    parser.process(a);
+
+    if (!parser.isSet(appIdOpt) || !parser.isSet(idOpt))
+        parser.showHelp();
+
+    QString appId = parser.value(appIdOpt);
+    QString id = parser.value(idOpt);
+
+    OpenWeather *openWeather = new OpenWeather(nullptr, appId);
     openWeather->showFullScreen();
-
-    ret = a.exec();
+    int ret = a.exec();
 
     delete openWeather;
 
