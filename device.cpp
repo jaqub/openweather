d8635ebf2dea146e4837d45fa703f004dbb4dbed
jaqub <jakub.biwel@gmail.com>
2018-03-03 19:54:10 +0100

    [SRC] Add support for device name.
    This gives us better debug log messages - containing device name.

--- device.cpp
+++ device.cpp
@@ -5,6 +5,7 @@
 Device::Device(udev_device *aDevice) : mDevice(aDevice)
 {
     Q_CHECK_PTR(aDevice);
+    mName = udev_device_get_sysname(mDevice);
 }
 
 QStringList Device::getAttrNameList()
@@ -14,13 +15,13 @@ QStringList Device::getAttrNameList()
 
    attrs = udev_device_get_sysattr_list_entry(mDevice);
    if (attrs == nullptr) {
-      qDebug() << "ATTRs for device not found";
+      qWarning() << "ATTR list for" << mName << "not found";
       return attrList;
    }
 
    udev_list_entry_foreach(attr, attrs) {
       const char *attrName = udev_list_entry_get_name(attr);
-      qDebug() << "Found ATTR[" << attrName << "]";
+      qDebug() << mName << "- found ATTR:" << attrName;
       attrList.append(attrName);
    }
 
@@ -30,13 +31,15 @@ QStringList Device::getAttrNameList()
 QByteArray Device::getAttrValue(char *aAttrName)
 {
     if (aAttrName == nullptr) {
-        qDebug() << "Attr name set to null";
+        qInfo() << "Attr name set to null";
         return nullptr;
     }
 
     const char *value = udev_device_get_sysattr_value(mDevice, aAttrName);
     if (value == nullptr)
         qWarning() << aAttrName << "not found";
+
+    qDebug() << mName << "ATTR:" << aAttrName << ":" << value;
 
     return value;
 }
@@ -46,6 +49,8 @@ int Device::setAttrValue(char *aAttrName, char *aValue)
     int ret = udev_device_set_sysattr_value(mDevice, aAttrName, aValue);
     if (ret < 0)
         qWarning() << "Failed to set" << aAttrName << "with" << aValue;
+
+    qDebug() << mName << "ATTR:" << aAttrName << "set to" << aValue;
 
     return ret;
 }
