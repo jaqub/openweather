58963b96007db0445ba24b5054ba8ea5a14e9413
jaqub <jakub.biwel@gmail.com>
2018-03-15 18:04:23 +0100

    [SRC] Reurn only one Device from getDeviceBySysname
    Previously intrface returns all find devices from which only
    one was choosen. Since we are looking for specified device
    (with specific sysname) we are only interested in that one
    device, not all.

--- udev.cpp
+++ udev.cpp
@@ -8,36 +8,36 @@ Udev::Udev()
     Q_CHECK_PTR(mUdev);
 }
 
-QVector<Device *> Udev::getDeviceBySysname(const char *aSysName, const char *aSubsytem)
+Device* Udev::getDeviceBySysname(const char *aSysName, const char *aSubsytem)
 {
-    QVector<Device *> devices;
+    Device *dev = nullptr;
 
     // Acquire enumerate object
     udev_enumerate *enumerate = udev_enumerate_new(mUdev);
     Q_CHECK_PTR(enumerate);
     if (enumerate == nullptr) {
         qWarning() << "Failed to get enumerate object";
-        return devices;
+        return nullptr;
     }
 
     //Add filters
     if (udev_enumerate_add_match_sysname(enumerate, aSysName) < 0) {
         qWarning() << "Failed to add" << aSysName << "to match data";
         udev_enumerate_unref(enumerate);
-        return devices;
+        return nullptr;
     }
 
     if (aSubsytem && (udev_enumerate_add_match_subsystem(enumerate, aSubsytem) < 0)) {
         qWarning() << "Failed to add" << aSubsytem << "to match data";
         udev_enumerate_unref(enumerate);
-        return devices;
+        return nullptr;
     }
 
     // Find devices with specified sys name
     if (udev_enumerate_scan_devices(enumerate) < 0) {
         qWarning() << "Failed to scan devices";
         udev_enumerate_unref(enumerate);
-        return devices;
+        return nullptr;
     }
 
     // Iterate over found devices and add them do devices
@@ -45,18 +45,31 @@ QVector<Device *> Udev::getDeviceBySysname(const char *aSysName, const char *aSu
     items = udev_enumerate_get_list_entry(enumerate);
     udev_list_entry_foreach(item, items) {
         const char *devName = udev_list_entry_get_name(item);
+        if (devName == nullptr)
+            continue;
 
-        qDebug() << "Found device:" << devName;
+        udev_device *uDev = udev_device_new_from_syspath(mUdev, devName);
+        if (uDev == nullptr)
+            continue;
 
-        udev_device *device = udev_device_new_from_syspath(mUdev, devName);
-        devices.append(new Device(device));
+        const char* uDevSysName = udev_device_get_sysname(uDev);
+        if (uDevSysName == nullptr) {
+            udev_device_unref(uDev);
+            continue;
+        }
+
+        if (!qstrncmp(aSysName, uDevSysName, qstrlen(aSysName))) {
+            qDebug() << "Found device:" << devName;
+            dev = new Device(uDev);
+            break;
+        }
+
+        udev_device_unref(uDev);
     }
-
-    qDebug() << "Found:" << devices.size() << "devices";
 
     // Release enumerate object and return devices found
     udev_enumerate_unref(enumerate);
-    return devices;
+    return dev;
 }
 
 QVector<Device *> Udev::getDevicesFromSubsystem(const char *aSubsystem)
